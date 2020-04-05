dfb63500db0a723729d8d68b4587cb03ea0064ba
lalitadithya1996@hotmail.com <lalitadithya1996@hotmail.com>
2020-04-05 11:00:53 +0530

    Added models

---
+++ LinqCustomProvider/Models/FileSystemElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqCustomProvider.Models
+{
+    public enum ElementType
+    {
+        File, 
+        Folder
+    }
+
+    public class FileSystemElement
+    {
+        public string Path { get; set; }
+        public ElementType ElementType { get; set; }
+
+        public FileSystemElement(string path)
+        {
+            Path = path;
+        }
+    }
+}
