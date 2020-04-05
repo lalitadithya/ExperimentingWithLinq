e8c45a78257c4763c2d7b3377cc1f9cf7f88f8b3
lalitadithya1996@hotmail.com <lalitadithya1996@hotmail.com>
2020-04-05 10:58:22 +0530

    Add project files.

---
+++ LinqCustomProvider/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqCustomProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
