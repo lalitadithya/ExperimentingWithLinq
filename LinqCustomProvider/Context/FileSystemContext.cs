2d0ec7b1652484c5009be8f7b28e58bac91cddc1
lalitadithya1996@hotmail.com <lalitadithya1996@hotmail.com>
2020-04-05 11:04:38 +0530

    Added context

---
+++ LinqCustomProvider/Context/FileSystemContext.cs
@@ -0,0 +1,35 @@
+﻿using LinqCustomProvider.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace LinqCustomProvider.Context
+{
+    public class FileSystemContext : IOrderedQueryable<FileSystemElement>
+    {
+        public Type ElementType
+        {
+            get
+            {
+                return typeof(FileSystemElement);
+            }
+        }
+
+        public Expression Expression { get; private set; }
+
+        public IQueryProvider Provider { get; private set; }
+
+        public IEnumerator<FileSystemElement> GetEnumerator()
+        {
+            return Provider.Execute<IEnumerable<FileSystemElement>>(Expression).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
